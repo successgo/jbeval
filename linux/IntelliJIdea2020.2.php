d0fade9d43fb6a109b894eabefe99abba8dabb67
Success Go <success.go@gaodacheng.com>
2020-11-08 16:47:10 +0800

    add idea

---
+++ linux/IntelliJIdea2020.2.php
@@ -0,0 +1,21 @@
+<?php
+
+$home_dir = getenv('HOME');
+$ide_path = $home_dir . '/.config/JetBrains/IntelliJIdea2020.2';
+
+// 1 remove eval/*.key
+$key = $ide_path . '/eval/idea202.evaluation.key';
+if (file_exists($key)) {
+    unlink($key);
+}
+
+// 2 remove all properties named `evl*` in options/other.xml
+exec("sed -i /evl.*/d {$ide_path}/options/other.xml");
+
+// 3 clear cache
+exec("rm -rf {$home_dir}/.java/.userPrefs/jetbrains/idea");
+
+// 4 clear all device id related with jetbrains
+exec("sed -i /JetBrains.UserIdOnMachine/d {$home_dir}/.java/.userPrefs/prefs.xml");
+exec("sed -i /device_id/d {$home_dir}/.java/.userPrefs/jetbrains/prefs.xml");
+exec("sed -i /user_id_on_machine/d {$home_dir}/.java/.userPrefs/jetbrains/prefs.xml");
